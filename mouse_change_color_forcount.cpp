219eb35e5c59a2e4e86f2cb6fbc7dc74ce3d3a92
higashi000 <higashi.ict@gmail.com>
2018-05-20 22:46:01 +0900

    クリックしたら色が変わるや〜つ

---
+++ mouse_change_color_forcount.cpp
@@ -0,0 +1,30 @@
+#include <Siv3D.hpp>
+
+void Main() {
+
+  const Rect rect1(20, 20, 100, 100);
+  const Rect rect2(120, 120, 100, 100);
+
+
+  int colorchange1 = 0;
+  int colorchange2 = 0;
+  int count1 = 0;
+  int count2 = 0;
+
+  while (System::Update()) {
+
+    if (rect1.leftPressed() == 1) {
+      count1++;
+    }
+    if (rect2.leftPressed() == 1) {
+      count2++;
+    }
+
+    if (count1 % 3 == 0) rect1.draw(Palette::White);
+    if (count1 % 3 == 1) rect1.draw(Palette::Red);
+    if (count1 % 3 == 2) rect1.draw(Palette::Blue);
+    if (count2 % 3 == 0) rect2.draw(Palette::White);
+    if (count2 % 3 == 1) rect2.draw(Palette::Red);
+    if (count2 % 3 == 2) rect2.draw(Palette::Blue);
+  }
+}
